77b2754a0521184d9e45968d76bcd355e4c54ad0
Gabriel Santos <ebert.gabrielst@gmail.com>
2024-12-19 23:49:11 -0300

    Mobile

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -14,14 +14,14 @@ export function Dashboard() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
           <MonthlyIncomeCard />
           <MonthlyOrderNumberCard />
           <DailyOrderNumberCard />
           <MonthCanceledOrderNumberCard />
         </div>
 
-        <div className="grid grid-cols-9 gap-4">
+        <div className="grid gap-4 sm:grid-cols-8 lg:grid-cols-9">
           <RevenueChart />
           <PopularProductsChart />
         </div>

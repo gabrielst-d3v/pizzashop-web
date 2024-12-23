b491d21e80b49a19dca53253bd182d29460b4f82
Gabriel Santos <ebert.gabrielst@gmail.com>
2024-12-23 20:45:11 -0300

    Update

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router'
 export function AuthLayout() {
   return (
     <div className="flex min-h-screen flex-col antialiased lg:grid lg:grid-cols-2">
-      <div className="flex flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground lg:h-full">
+      <div className="flex items-center justify-center bg-muted p-4 text-muted-foreground sm:items-center sm:justify-between sm:border-r sm:p-10 lg:flex lg:h-full lg:flex-col lg:border-foreground/5">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
           <span className="font-semibold">Pizza.shop</span>
@@ -14,7 +14,7 @@ export function AuthLayout() {
         </footer>
       </div>
 
-      <div className="relative grid items-center justify-center p-4 py-20 sm:items-center sm:justify-center sm:p-10 lg:flex lg:flex-col">
+      <div className="sm:p-15 relative grid items-center justify-center p-4 py-24 sm:items-center sm:justify-center lg:flex lg:flex-col">
         <Outlet />
       </div>
     </div>
